f7c4a075f47d50fed06459b018aecfa847f5a731
alessandra cappati <alessandra.cappati@cern.ch>
2023-02-17 18:29:48 +0100

    compute event loss percentage

--- computeTriggerEffect.C
+++ computeTriggerEffect.C
@@ -15,12 +15,15 @@
 #include "TMath.h"
 
 
+#define REDOHISTOS 0
 
-void readFiles(){
 
-  
-  const int nDatasets = 5;
-  TString samplesName[nDatasets] = {"ggH125", "VBFH125", "WminusH125", "WplusH125", "ZH125"}; //, "ttH125", "bbH125"};
+
+const int nDatasets = 5;
+TString samplesName[nDatasets] = {"ggH125", "VBFH125", "WminusH125", "WplusH125", "ZH125"}; //, "ttH125", "bbH125"};
+
+
+void makeHistos(){
 
   // --- define input file characteristics
   TString rootInDir = "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/Run2UL_22/Hadded/MC/MC2016preVFP/";
@@ -75,20 +78,20 @@ void readFiles(){
       hEvents_eta2p5[d]->Fill(0.5, 1.);     
 
       // -2.4 < eta < 2.4
-      if((TMath::Abs(LepEta->at(0))<2.4) && TMath::Abs(LepLepId->at(0))==11){
+      if((TMath::Abs(LepEta->at(0))<2.4 && TMath::Abs(LepLepId->at(0))==11) || TMath::Abs(LepLepId->at(0))==13){
           hEvents_eta2p4_single[d]->Fill(0.5, 1.);     
       }
 
-      if((TMath::Abs(LepEta->at(0))<2.4 && TMath::Abs(LepLepId->at(0))==11) && (TMath::Abs(LepEta->at(1))<2.4 && TMath::Abs(LepLepId->at(1))==11)){
+      if(((TMath::Abs(LepEta->at(0))<2.4 && TMath::Abs(LepLepId->at(0))==11) || TMath::Abs(LepLepId->at(0))==13) && ((TMath::Abs(LepEta->at(1))<2.4 && TMath::Abs(LepLepId->at(1))==11) || TMath::Abs(LepLepId->at(1))==13)){
           hEvents_eta2p4_double[d]->Fill(0.5, 1.);     
       }
       
       // -2.3 < eta < 2.3
-      if((TMath::Abs(LepEta->at(0))<2.3) && TMath::Abs(LepLepId->at(0))==11){
+      if((TMath::Abs(LepEta->at(0))<2.3 && TMath::Abs(LepLepId->at(0))==11) || TMath::Abs(LepLepId->at(0))==13){
           hEvents_eta2p3_single[d]->Fill(0.5, 1.);     
       }
 
-      if((TMath::Abs(LepEta->at(0))<2.3 && TMath::Abs(LepLepId->at(0))==11) && (TMath::Abs(LepEta->at(1))<2.3 && TMath::Abs(LepLepId->at(1))==11)){
+      if(((TMath::Abs(LepEta->at(0))<2.3 && TMath::Abs(LepLepId->at(0))==11) || TMath::Abs(LepLepId->at(0))==13) && ((TMath::Abs(LepEta->at(1))<2.3 && TMath::Abs(LepLepId->at(1))==11) || TMath::Abs(LepLepId->at(1))==13)){
           hEvents_eta2p3_double[d]->Fill(0.5, 1.);     
       }
 
@@ -117,9 +120,100 @@ void readFiles(){
 
 
 
+void printYields(){
+
+  TFile* finput = TFile::Open("histos.root");
+  
+  TH1F* hEvents_eta2p5       [nDatasets];
+  TH1F* hEvents_eta2p4_single[nDatasets];
+  TH1F* hEvents_eta2p3_single[nDatasets];
+  TH1F* hEvents_eta2p4_double[nDatasets];
+  TH1F* hEvents_eta2p3_double[nDatasets];
+
+  cout<<"Retrieving histos.."<<endl;
+  for(int d=0; d<nDatasets; d++){
+    hEvents_eta2p5[d]       = (TH1F*)finput->Get("hEvents_eta2p5_"+samplesName[d]);
+    hEvents_eta2p4_single[d]= (TH1F*)finput->Get("hEvents_eta2p4_single_"+samplesName[d]);
+    hEvents_eta2p3_single[d]= (TH1F*)finput->Get("hEvents_eta2p3_single_"+samplesName[d]);
+    hEvents_eta2p4_double[d]= (TH1F*)finput->Get("hEvents_eta2p4_double_"+samplesName[d]);
+    hEvents_eta2p3_double[d]= (TH1F*)finput->Get("hEvents_eta2p3_double_"+samplesName[d]);
+  }
+
+  
+  Float_t evt_now_ggH = hEvents_eta2p5[0]->GetBinContent(1);
+  Float_t evt_now_VBF = hEvents_eta2p5[1]->GetBinContent(1);
+  Float_t evt_now_VH  = hEvents_eta2p5[2]->GetBinContent(1) + hEvents_eta2p5[3]->GetBinContent(1) + hEvents_eta2p5[4]->GetBinContent(1); // WplusH + WminusH + ZH
+  Float_t evt_now_tot = evt_now_ggH + evt_now_VBF + evt_now_VH;
+
+  Float_t evt_singEle2p4_ggH = hEvents_eta2p4_single[0]->GetBinContent(1);
+  Float_t evt_singEle2p4_VBF = hEvents_eta2p4_single[1]->GetBinContent(1);
+  Float_t evt_singEle2p4_VH  = hEvents_eta2p4_single[2]->GetBinContent(1) + hEvents_eta2p4_single[3]->GetBinContent(1) + hEvents_eta2p4_single[4]->GetBinContent(1); // WplusH + WminusH + ZH
+  Float_t evt_singEle2p4_tot = evt_singEle2p4_ggH + evt_singEle2p4_VBF + evt_singEle2p4_VH;
+
+  Float_t evt_doubEle2p4_ggH = hEvents_eta2p4_double[0]->GetBinContent(1);
+  Float_t evt_doubEle2p4_VBF = hEvents_eta2p4_double[1]->GetBinContent(1);
+  Float_t evt_doubEle2p4_VH  = hEvents_eta2p4_double[2]->GetBinContent(1) + hEvents_eta2p4_double[3]->GetBinContent(1) + hEvents_eta2p4_double[4]->GetBinContent(1); // WplusH + WminusH + ZH
+  Float_t evt_doubEle2p4_tot = evt_doubEle2p4_ggH + evt_doubEle2p4_VBF + evt_doubEle2p4_VH;
+
+  Float_t evt_singEle2p3_ggH = hEvents_eta2p3_single[0]->GetBinContent(1);
+  Float_t evt_singEle2p3_VBF = hEvents_eta2p3_single[1]->GetBinContent(1);
+  Float_t evt_singEle2p3_VH  = hEvents_eta2p3_single[2]->GetBinContent(1) + hEvents_eta2p3_single[3]->GetBinContent(1) + hEvents_eta2p3_single[4]->GetBinContent(1); // WplusH + WminusH + ZH
+  Float_t evt_singEle2p3_tot = evt_singEle2p3_ggH + evt_singEle2p3_VBF + evt_singEle2p3_VH;
+
+  Float_t evt_doubEle2p3_ggH = hEvents_eta2p3_double[0]->GetBinContent(1);
+  Float_t evt_doubEle2p3_VBF = hEvents_eta2p3_double[1]->GetBinContent(1);
+  Float_t evt_doubEle2p3_VH  = hEvents_eta2p3_double[2]->GetBinContent(1) + hEvents_eta2p3_double[3]->GetBinContent(1) + hEvents_eta2p3_double[4]->GetBinContent(1); // WplusH + WminusH + ZH
+  Float_t evt_doubEle2p3_tot = evt_doubEle2p3_ggH + evt_doubEle2p3_VBF + evt_doubEle2p3_VH;
+
+
+  // print stuff
+  cout<<"    "<<"Now      "<<"  "<<"singleEle |eta|<2.4  "<<"  "<<"doubleEle |eta|<2.4  "<<"  "<<"singleEle |eta|<2.3  "<<"  "<<"doubleEle |eta|<2.3  "<<endl;
+  cout<<"ggH "<<evt_now_ggH<<"  "<<evt_singEle2p4_ggH     <<"  "<<evt_doubEle2p4_ggH     <<"  "<<evt_singEle2p3_ggH     <<"  "<<evt_doubEle2p3_ggH     <<endl;
+  cout<<"VBF "<<evt_now_VBF<<"  "<<evt_singEle2p4_VBF     <<"  "<<evt_doubEle2p4_VBF     <<"  "<<evt_singEle2p3_VBF     <<"  "<<evt_doubEle2p3_VBF     <<endl;
+  cout<<"VH  "<<evt_now_VH <<"  "<<evt_singEle2p4_VH      <<"  "<<evt_doubEle2p4_VH      <<"  "<<evt_singEle2p3_VH      <<"  "<<evt_doubEle2p3_VH      <<endl;
+  cout<<"Tot "<<evt_now_tot<<"  "<<evt_singEle2p4_tot     <<"  "<<evt_doubEle2p4_tot     <<"  "<<evt_singEle2p3_tot     <<"  "<<evt_doubEle2p3_tot     <<endl;
+
+
+  // compute percentage of lost events
+  Float_t ploss_singEle2p4_ggH = ((evt_now_ggH - evt_singEle2p4_ggH) / evt_now_ggH) * 100.;
+  Float_t ploss_singEle2p4_VBF = ((evt_now_VBF - evt_singEle2p4_VBF) / evt_now_VBF) * 100.;
+  Float_t ploss_singEle2p4_VH  = ((evt_now_VH  - evt_singEle2p4_VH)  / evt_now_VH ) * 100.;
+  Float_t ploss_singEle2p4_tot = ((evt_now_tot - evt_singEle2p4_tot) / evt_now_tot) * 100.;
+
+  Float_t ploss_doubEle2p4_ggH = ((evt_now_ggH - evt_doubEle2p4_ggH) / evt_now_ggH) * 100.;
+  Float_t ploss_doubEle2p4_VBF = ((evt_now_VBF - evt_doubEle2p4_VBF) / evt_now_VBF) * 100.;
+  Float_t ploss_doubEle2p4_VH  = ((evt_now_VH  - evt_doubEle2p4_VH)  / evt_now_VH ) * 100.;
+  Float_t ploss_doubEle2p4_tot = ((evt_now_tot - evt_doubEle2p4_tot) / evt_now_tot) * 100.;
+
+  Float_t ploss_singEle2p3_ggH = ((evt_now_ggH - evt_singEle2p3_ggH) / evt_now_ggH) * 100.;
+  Float_t ploss_singEle2p3_VBF = ((evt_now_VBF - evt_singEle2p3_VBF) / evt_now_VBF) * 100.;
+  Float_t ploss_singEle2p3_VH  = ((evt_now_VH  - evt_singEle2p3_VH)  / evt_now_VH ) * 100.;
+  Float_t ploss_singEle2p3_tot = ((evt_now_tot - evt_singEle2p3_tot) / evt_now_tot) * 100.;
+
+  Float_t ploss_doubEle2p3_ggH = ((evt_now_ggH - evt_doubEle2p3_ggH) / evt_now_ggH) * 100.;
+  Float_t ploss_doubEle2p3_VBF = ((evt_now_VBF - evt_doubEle2p3_VBF) / evt_now_VBF) * 100.;
+  Float_t ploss_doubEle2p3_VH  = ((evt_now_VH  - evt_doubEle2p3_VH)  / evt_now_VH ) * 100.;
+  Float_t ploss_doubEle2p3_tot = ((evt_now_tot - evt_doubEle2p3_tot) / evt_now_tot) * 100.;
+
+  // print stuff
+  cout<<"    "<<"singleEle|eta|<2.4  "               <<"doubleEle|eta|<2.4  "             <<"singleEle|eta|<2.3  "             <<"doubleEle|eta|<2.3  "          <<endl;
+  cout<<"ggH "<<ploss_singEle2p4_ggH <<" %          "<<ploss_doubEle2p4_ggH <<" %        "<<ploss_singEle2p3_ggH <<" %        "<<ploss_doubEle2p3_ggH    <<" %  "<<endl;
+  cout<<"VBF "<<ploss_singEle2p4_VBF <<" %          "<<ploss_doubEle2p4_VBF <<" %        "<<ploss_singEle2p3_VBF <<" %        "<<ploss_doubEle2p3_VBF	 <<" %  "<<endl;
+  cout<<"VH  "<<ploss_singEle2p4_VH  <<" %          "<<ploss_doubEle2p4_VH  <<" %        "<<ploss_singEle2p3_VH  <<" %        "<<ploss_doubEle2p3_VH 	 <<" %  "<<endl;
+  cout<<"Tot "<<ploss_singEle2p4_tot <<" %          "<<ploss_doubEle2p4_tot <<" %        "<<ploss_singEle2p3_tot <<" %        "<<ploss_doubEle2p3_tot	 <<" %  "<<endl;
+
+}
+
+
+
 
 void computeTriggerEffect(){
 
-  readFiles();
+  if(REDOHISTOS){
+    makeHistos();
+  }
+
+  printYields();
+  
 
 }
